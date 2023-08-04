2985f3c58f8a96d15c7ab82113eedff2b5e3803f
frankcpa <frankcpa84@gmail.com>
2023-08-04 14:09:30 -0400

    com o listar funcionando e a interface do cliente

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,11 +2,24 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election{
-    //Store candidate
-    //Read candidate
-    string public candidate;
-    // constructor
-    constructor() public{
-        candidate = "Candidate 1";
+    // Model a Candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voreCount;
+    }
+    // Read/write Candidates
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private{
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
