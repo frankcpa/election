a17a5487acc64d1df0f15f2bfcd2cb1e8e474823
frankcpa <frankcpa84@gmail.com>
2023-08-04 09:12:17 -0400

    teste inicial

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election{
+    //Store candidate
+    //Read candidate
+    string public candidate;
+    // constructor
+    constructor() public{
+        candidate = "Candidate 1";
+    }
+}
